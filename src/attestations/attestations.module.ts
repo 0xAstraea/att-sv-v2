a66a29abb8ea397f8b1221b8cc1e00596d36e866
0xAstraea <marazza.matias@gmail.com>
2024-11-24 23:07:55 +0700

    Add config to att module

--- src/attestations/attestations.module.ts
+++ src/attestations/attestations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AttestationsService } from './attestations.service';
 import { AttestationsController } from './attestations.controller';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [AttestationsController],
   providers: [AttestationsService],
 })
